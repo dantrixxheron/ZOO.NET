b1d2cd54ff345dde1acad2a02483ae658d1fbb04
Daniela Cruz Mejía <123124430+dantrixxheron@users.noreply.github.com>
2023-07-23 01:39:39 +0000

    added link to edit this repository

--- js/directions.js
+++ js/directions.js
@@ -14,5 +14,5 @@ function MoreInfo(animal){
     if(animal == "tortugaM"){window.open(`https://www.nationalgeographicla.com/search?q=tortugas&type=article`, '_blank');}
     if(animal == "flamenco"){window.open(`https://youtu.be/0gZkQkKmlgo`, '_blank');}
     if(animal == "help"){window.open(`https://preserve.nature.org/page/85801/donate/1?locale=es-MX&button=nature.org`, '_blank');}
-    if(animal == "edit"){window.open(``, '_blank');}
+    if(animal == "edit"){window.open(`https://github.com/dantrixxheron/ZOO.NET`, '_blank');}
 }
